e931cf66880d96f255009c35a4817c93aa0dad01
Ragini340 <ragini31bxr@gmail.com>
2022-10-26 23:22:40 +0530

    Add project files.

---
+++ RestAPIDemoDemo/Controllers/PeopleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPIDemoDemo.Model;
+
+namespace RestAPIDemoDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> GetAll()
+        {
+            return new[]
+            {
+            new Person { Name = "Alica" },
+            new Person { Name = "Zack" },
+            new Person { Name = "Will" }
+        };
+        }
+    }
+}
+
---
+++ RestAPIDemoDemo/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using RestAPIDemoDemo.Model;
+
+namespace RestAPIDemoDemo.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        
+    }
+    
+}
